9055c640cc8b388b00e8985dafe89f77cc04afc6
vincent Lafosse <v.lafosse@sfeir.com>
2021-11-18 02:04:23 +0100

    Réalisation d'un papier pour sur les requêtte sql avec go. (version pure)

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/ileossa/go-bank-backend
 
 go 1.17
 
-require github.com/go-chi/chi v1.5.4
+require (
+	github.com/lib/pq v1.10.4 // indirect
+)
--- main.go
+++ main.go
@@ -1,31 +1,138 @@
+// **** file: main.go
 package main
 
 import (
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
-	"os"
+	"strconv"
+
+	_ "github.com/lib/pq"
 )
 
-func main() {
+type Book struct {
+	isbn   string
+	title  string
+	author string
+	price  float32
+}
 
-	port := "8080"
+var db *sql.DB
 
-	if fromEnv := os.Getenv("PORT"); fromEnv != "" {
-		port = fromEnv
+func init() {
+	var err error
+	db, err = sql.Open("postgres", "postgres://postgres:changeme@localhost:5432/bookstore?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	log.Printf("Starting up on http://localhost:%s", port)
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+}
 
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
+func main() {
+	http.HandleFunc("/books", booksIndex)
+	http.HandleFunc("/books/show", bookShow)
+	http.HandleFunc("/books/create", bookCreate)
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain")
-		w.Write([]byte("Hello World!"))
-	})
+	http.ListenAndServe(":3000", nil)
+	fmt.Sprintln("Server launch on http://localhost:3000")
+}
 
-	r.Mount("/posts", postsResource{}.Routes())
-	log.Fatal(http.ListenAndServe(":"+port, r))
+func booksIndex(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), 405)
+		return
+	}
+
+	rows, err := db.Query("SELECT * FROM books")
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+	defer rows.Close()
+
+	bks := make([]*Book, 0)
+	for rows.Next() {
+		bk := new(Book)
+		err := rows.Scan(&bk.isbn, &bk.title, &bk.author, &bk.price)
+		if err != nil {
+			http.Error(w, http.StatusText(500), 500)
+			return
+		}
+		bks = append(bks, bk)
+	}
+	if err = rows.Err(); err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	for _, bk := range bks {
+		fmt.Fprintf(w, "%s, %s, %s, %.2f€\n", bk.isbn, bk.title, bk.author, bk.price)
+	}
+}
+
+func bookShow(w http.ResponseWriter, r *http.Request) {
+
+	if "GET" != r.Method {
+		http.Error(w, http.StatusText(405), 405)
+		return
+	}
+
+	isbn := r.FormValue("isbn")
+	if "" == isbn {
+		http.Error(w, http.StatusText(400), 400)
+		return
+	}
+
+	rows := db.QueryRow("SELECT * FROM books WHERE isbn = $1", isbn)
+
+	book := new(Book)
+	err := rows.Scan(&book.isbn, &book.title, &book.author, &book.price)
+	if err == sql.ErrNoRows {
+		http.NotFound(w, r)
+		return
+	} else if nil != err {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	fmt.Fprintf(w, "%s, %s, %s, %.2f€\n", book.isbn, book.title, book.author, book.price)
+}
+
+func bookCreate(w http.ResponseWriter, r *http.Request) {
+
+	if "POST" != r.Method {
+		http.Error(w, http.StatusText(405), 405)
+		return
+	}
+
+	isbn := r.FormValue("isbn")
+	title := r.FormValue("title")
+	author := r.FormValue("author")
+	if isbn == "" || title == "" || author == "" {
+		http.Error(w, http.StatusText(400), 400)
+		return
+	}
+	price, err := strconv.ParseFloat(r.FormValue("price"), 32)
+	if err != nil {
+		http.Error(w, http.StatusText(400), 400)
+		return
+	}
+
+	result, err := db.Exec("INSERT INTO books VALUES($1, $2, $3, $4)", isbn, title, author, price)
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	fmt.Fprintf(w, "Book %s created successfully (%d row affected)\n", isbn, rowsAffected)
 }
